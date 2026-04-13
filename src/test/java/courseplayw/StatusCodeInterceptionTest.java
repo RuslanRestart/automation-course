86e0cee99eb6b3656987acf25ccc2f112c471d6a
erofeev.ruslan2 <erofeev.ruslan2@rwb.ru>
2026-04-14 01:50:07 +0300

    Практическое задание №1

--- src/test/java/courseplayw/StatusCodeInterceptionTest.java
+++ src/test/java/courseplayw/StatusCodeInterceptionTest.java
@@ -8,7 +8,7 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-public class NetworkInterceptionTest extends BaseTest {
+public class StatusCodeInterceptionTest extends BaseTest {
 
     @Test
     public void mockResponseTest(){
