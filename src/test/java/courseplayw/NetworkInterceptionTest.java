69c8add51bc3e65726f8ad497c0d8213a43eeaf9
erofeev.ruslan2 <erofeev.ruslan2@rwb.ru>
2026-04-14 01:39:57 +0300

    add mock herokuapp response test

---
+++ src/test/java/courseplayw/NetworkInterceptionTest.java
@@ -0,0 +1,30 @@
+package courseplayw;
+
+import base.BaseTest;
+import com.microsoft.playwright.Page;
+import com.microsoft.playwright.Route;
+import com.microsoft.playwright.options.AriaRole;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class NetworkInterceptionTest extends BaseTest {
+
+    @Test
+    public void mockResponseTest(){
+        String url = "https://the-internet.herokuapp.com/status_codes";
+        String mockedBody = "Mocked Success Response";
+
+        page.route(url + "/404", route -> {
+            route.fulfill(new Route.FulfillOptions()
+                    .setStatus(200)
+                    .setContentType("application/json")
+                    .setBody(mockedBody));
+                }
+        );
+        page.navigate(url);
+        page.getByRole(AriaRole.LINK, new Page.GetByRoleOptions().setName("404")).click();
+        assertTrue(page.textContent("body").contains(mockedBody), "Mocked message is not displayed!");
+
+    }
+}
