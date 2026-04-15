20d2ad8d04b6ad4c539ff969632506e40058a936
erofeev.ruslan2 <erofeev.ruslan2@rwb.ru>
2026-04-15 17:18:47 +0300

    Практическое задание №4

--- src/test/java/base/BaseTest.java
+++ src/test/java/base/BaseTest.java
@@ -1,9 +1,14 @@
 package base;
 
 import com.microsoft.playwright.*;
+import io.qameta.allure.Allure;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.extension.ExtensionContext;
+import org.junit.jupiter.api.extension.RegisterExtension;
+import org.junit.jupiter.api.extension.TestWatcher;
 
+import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -17,6 +22,8 @@ public class BaseTest {
     BrowserContext context;
     public Page page;
     protected String timestamp;
+    protected Throwable testException;
+
 
     @BeforeEach
     void setUp() throws IOException {
@@ -30,11 +37,28 @@ public class BaseTest {
         page = context.newPage();
     }
 
+    @RegisterExtension
+    TestWatcher watcher = new TestWatcher() {
+        @Override
+        public void testFailed(ExtensionContext context, Throwable cause) {
+            testException = cause;
+        }
+    };
 
 
     @AfterEach
     void tearDown() {
+        if (testException != null && page != null && !page.isClosed()) {
+            byte[] screenshot = page.screenshot();
+            Allure.addAttachment(
+                    "Screenshot on Failure",
+                    "image/png",
+                    new ByteArrayInputStream(screenshot),
+                    ".png"
+            );
+        }
         context.close();
         playwright.close();
+        testException = null;
     }
 }
