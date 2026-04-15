f58f8958fa21fb359e7343beba145c54b010723e
erofeev.ruslan2 <erofeev.ruslan2@rwb.ru>
2026-04-15 18:17:16 +0300

    Практическое задание №5

--- src/test/java/courseplayw/CartTest.java
+++ src/test/java/courseplayw/CartTest.java
@@ -6,8 +6,12 @@ import com.microsoft.playwright.Page;
 import com.microsoft.playwright.options.AriaRole;
 import org.junit.jupiter.api.Test;
 
+import java.io.IOException;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class CartTest extends BaseTest {
 
@@ -25,6 +29,23 @@ public class CartTest extends BaseTest {
             page.locator("#content").screenshot(new Locator.ScreenshotOptions()
                     .setPath(getTimestampPath("cart_after_remove.png")));
         }
+
+    @Test
+    void testHomePageVisual() throws IOException {
+        page.navigate("https://the-internet.herokuapp.com");
+
+        Path actual = Paths.get("actual.png");
+        Path expected = Paths.get("expected.png");
+
+        page.screenshot(new Page.ScreenshotOptions().setPath(actual));
+        long mismatch = Files.mismatch(actual, expected);
+
+        if (mismatch != -1) {
+            System.out.println("Есть различия!");
+        }
+
+        assertThat(mismatch).isEqualTo(-1);
+    }
 
         private Path getTimestampPath(String filename) {
             return Paths.get(timestamp, filename);
